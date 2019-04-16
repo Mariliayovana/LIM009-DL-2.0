929e7d0e5c0820b1572e70d108d1e2f8370c252f
Mariliayovana <marilia_yovana@hotmail.com>
2019-04-16 13:02:56 -0500

    llamado de mi data a la consola

--- src/data.js
+++ src/data.js
@@ -3,8 +3,26 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
+const dataPotter = () => {
+  fetch('./data/potter.json')
+  .then(function(response) {
+    return response.json();
+  }).then(function(data){
+    pintarDatos(data)
+  })
+  
+
 };
 
-window.example = example;
+
+const pintarDatos = (data) => {
+  for(let i=0; i< data.length; i++){
+    
+    console.log(data[i].name)
+    console.log(data[i].species)
+  }
+
+}
+
+
+window.dataPotter = dataPotter;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 /* Manejo del DOM */
+window.dataPotter()
+
+/*const dataMuestra = (dataPotter)=>{
+  let news = "";
+  for(let i = 0; i<dataPotter.length; i++) {
+  let item = ´ 
+  <div"data">
+  <h2><a hret="${dataPotter[i].name}"target="_blank></a></h2>
+
+  </div>
+  ´
+
+
+};*/
+
