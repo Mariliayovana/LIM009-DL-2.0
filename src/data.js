8bce7053d4360ebabaf07b12f2b3047ba713a8bd
Mariliayovana <marilia_yovana@hotmail.com>
2019-04-22 01:19:06 -0500

    filtrando en la data

--- src/data.js
+++ src/data.js
@@ -2,28 +2,42 @@
 
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
+let personajes =[];
 const dataPotter = () => {
   fetch('./data/potter.json')
   .then(function(response) {
     return response.json();
   }).then(function(data){
-    pintarDatos(data)
+    filtrarDatos(data, "male", "staff", "Gryffindor")
+    //perosnaje = data;
   })
-
 
 };
 
+/*filtrar por cada personaje*/
+const filtrarDatos = (data, gender, role, house) => {
+  let filtrar = data;
+  //filtrar mi array
+// datap un elemento del array
+// si cumple la condición
+  if(gender)
+  filtrar = filtrar.filter(datap => datap.gender === gender)
+ 
+  if(house) 
+filtrar = filtrar.filter(datap => datap.house === house)
+  
+  if (role === "student")
+  filtrar = filtrar.filter(datap => datap.hogwartsStudent === true)
+   else if(role === "staff")
+  filtrar = filtrar.filter(datap => datap.hogwartsStaff === true) 
 
-const pintarDatos = (data) => {
-  for(let i=0; i< data.length; i++){
 
 
-    console.log(data[i].name)
-    console.log(data[i].species)
-  }
-
+ console.log(filtrar)
+  
 }
 
 
 window.dataPotter = dataPotter;
+window.personajes = personajes;
+window.filtrarDatos = filtrarDatos;
