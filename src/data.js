a0f86cfd76b117be4645547cfb33b98d5d3cd7a5
Mariliayovana <marilia_yovana@hotmail.com>
2019-04-25 00:28:31 -0500

    mostrando en la pantalla con main

--- src/data.js
+++ src/data.js
@@ -1,43 +1,12 @@
-/* Manejo de data */
+  /*const filtrarDatos = (POTTER, condición) => {
+  const filtrar = filtrar.filter(datap => datap.gender === condición);
+  return filtrar; 
+  }
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-let personajes =[];
-const dataPotter = () => {
-  fetch('./data/potter.json')
-  .then(function(response) {
-    return response.json();
-  }).then(function(data){
-    filtrarDatos(data, "male", "staff", "Gryffindor")
-    //perosnaje = data;
-  })
-
-};
-
-/*filtrar por cada personaje*/
-const filtrarDatos = (data, gender, role, house) => {
-  let filtrar = data;
-  //filtrar mi array
-// datap un elemento del array
-// si cumple la condición
-  if(gender)
-  filtrar = filtrar.filter(datap => datap.gender === gender)
- 
-  if(house) 
-filtrar = filtrar.filter(datap => datap.house === house)
+  const filtrar = (POTTER, condición) => {
+    const filtrar = POTTER.filter(element => element.rol === condición);
+    return filtrar; 
+    }
   
-  if (role === "student")
-  filtrar = filtrar.filter(datap => datap.hogwartsStudent === true)
-   else if(role === "staff")
-  filtrar = filtrar.filter(datap => datap.hogwartsStaff === true) 
 
-
-
- console.log(filtrar)
-  
-}
-
-
-window.dataPotter = dataPotter;
-window.personajes = personajes;
-window.filtrarDatos = filtrarDatos;
+*/
--- src/main.js
+++ src/main.js
@@ -1,4 +1,47 @@
 /* Manejo del DOM */
-window.dataPotter()
+ 
+/*const mostrarData = () => {
+  fetch('./data/potter.json')
+  .then((response => response.json() )
+  . then((data) =>{
+    dataPotter(data);
+    imprimirPorGenero(data);
+    filtrarDatos(data,'male')
+  }),
 
 
+const genero = document.getElementById('genero');
+const rol = document.getElementById('rol');
+const casa = document.getElementById('casa');
+*/
+const personajes = document.getElementById("personajes");
+
+const dataPotter =(data) =>{
+  let mostrar = ' ';
+  for(let i=0 ; i < data.length ; i++){
+     mostrar += `
+     <img src="${data[i].image}" class ="fotos">
+     <div> Nombre: ${data[i].name} </div>
+     <div> Casa: ${data[i].house} </div> 
+     <div> Especie: ${data[i].species}</div>
+     <div> Rol: roles(data) ${data[i].roles}</div>
+     <div> Edad: ${2019-data[i].yearOfBirth}</div>
+     <div> Actor:${data[i].actor}</div>
+    `}
+      return  personajes.innerHTML = mostrar;
+         
+  };
+  
+  //data es un objeto. ¿cómo voy al atributo de un objeto?
+  const roles =(data) =>{
+    let rol =[];
+    if (hogwartsStudent)
+    return estudiante
+else
+   if (hogwartsStaff == true)
+    return 
+   
+  }
+  console.log(dataPotter(POTTER));
+
+  
